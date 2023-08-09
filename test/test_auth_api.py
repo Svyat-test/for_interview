19ad5e06fe60e0a95a7d207a41ead0ad4b8fde67
PetrovSR <svyatoslav.petrov@vitech.team>
2023-08-09 15:59:15 +0300

    Подправил тест с негативным апи

--- test/test_auth_api.py
+++ test/test_auth_api.py
@@ -27,22 +27,13 @@ def test_get_auth_mock_positive(monkeypatch):
 
 
 @pytest.mark.api
-def test_get_auth_mock_negative(monkeypatch):
-    """
-    Тест на неуспешную авторизацию
-    :param monkeypatch:
-    :return: мокированные данные
-    """
-    class MockResponse(object):
-        def __init__(self):
-            self.status_code = 401
-            self.url = api_login()
+def test_get_auth_mock_negative():
+    request = get_auth(api_login(), login_negative())
+    assert request == 403, f'Получили: {request}, ожидали: 403'
 
-        def json(self):
-            return {'data': 'auth success'}
 
-    def mock_get(*args, **kwargs):
-        return MockResponse()
 
-    monkeypatch.setattr(requests, 'post', mock_get)
-    assert get_auth(api_login(), login_negative()) == 401
+
+
+
+
